6d8dd6f9edf3b82f012216c17baec711d960e5e6
jadhavabhiee <92208809+jadhavabhiee@users.noreply.github.com>
2022-02-06 07:54:54 +0530

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,19 @@
+// Write a program to have 3 variables ( all three are number) and output the highest/ greatest/ maximum of all three
+
+var a = 15 ;
+var b = 30 ;
+var c = 50 ;
+
+if (a > b)
+	 {if (a > c)
+		 {console.log("a is the greatest number");
+		 }else
+		 {console.log("c is the greatest number");}
+	 }else
+	 {if(b > c)
+		 {console.log("b is the greatest number");
+		 }else
+		 {console.log("c is the greatest number");}
+	 }
+	 
+ 
